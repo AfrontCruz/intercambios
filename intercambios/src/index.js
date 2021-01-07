a57ee184b404d2f922f92594fe563c329c502a38
AfrontCruz <danafront0@gmail.com>
2021-01-07 10:10:49 -0600

    First

---
+++ intercambios/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './routes/App';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const container = document.getElementById('root');
+
+ReactDOM.render(<App />, container);
---
+++ intercambios/src/routes/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+const App = () =>{
+    return(
+        <BrowserRouter>
+            <Switch>
+                
+            </Switch>
+        </BrowserRouter>
+    )
+};
+
+export default App;
