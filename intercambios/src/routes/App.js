b8354517af100b5b6769063bbc473cfe32df688e
AfrontCruz <danafront0@gmail.com>
2021-01-09 20:18:57 -0600

    First

--- intercambios/src/routes/App.js
+++ intercambios/src/routes/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import LoginContainer from '../containers/LoginContainer';
 
 const App = () =>{
     return(
         <BrowserRouter>
             <Switch>
-                
+                <Route exact path="/" >
+                    <LoginContainer />
+                    
+                </Route> 
             </Switch>
         </BrowserRouter>
     )
